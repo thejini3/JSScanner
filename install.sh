0998e10d13f5e186fd7dc5b9a22bf1b371599eae
thejini3 <thejini3@gmail.com>
2020-08-11 19:33:21 +0600

    changes

--- install.sh
+++ install.sh
@@ -12,16 +12,8 @@ else
         printf "LinkFinder already present in tools folder...!\n\n"
 fi
 
-sudo apt install wget -y
+
 
 cd LinkFinder
 
-sudo pip3 install -r requirements.txt
-sudo python3 setup.py install
-pip install jsbeautifier
-
-echo "alias jsscanner='$cwd/script.sh'" >> ~/.bash_profile
-
-. ~/.bash_profile
-
-echo "All set bro, restart your terminal!"
+pip3 install jsbeautifier
